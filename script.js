e49f6e8560c5238f97ce29dd66aac9b48560cef2
vsficoseco <vsficoseco@gmail.com>
2019-11-16 21:17:28 -0300

    get cat photos from api

---
+++ script.js
@@ -0,0 +1,14 @@
+function getCatByCategory(id) {
+  let url = `https://api.thecatapi.com/v1/images/search`;
+
+  if (id != 0) {
+    url = `${url}?category_ids=${id}`;
+  }
+
+  $.ajax({
+    method: "GET",
+    url: url
+  }).done(data => {
+    $("#imagen").html(`<img src='${data[0].url}' />`);
+  });
+}
